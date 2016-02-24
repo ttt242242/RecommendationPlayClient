6d3c1899d5aa063320756c98ce5d35f2b8fd57a0
ttt242242 <okano565656@gmail.com>
2016-02-24 09:08:23 +0900

    横スライドを可能にした

--- app/src/main/java/recommendation/okano/recommendationplayclient/MainActivity.java
+++ app/src/main/java/recommendation/okano/recommendationplayclient/MainActivity.java
@@ -1,16 +1,79 @@
 package recommendation.okano.recommendationplayclient;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentPagerAdapter adapterViewPager;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        FragmentManager manager = getSupportFragmentManager();
+//        manager.findFragmentByTag("android:switcher:");
+//        ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+//        MyFragmentStatePagerAdapter adapter = new MyFragmentStatePagerAdapter(manager);
+//        viewPager.setAdapter(adapter);
+//        //viewPager.setCurrentItem(0);
+//        Log.e("ddd", String.valueOf(viewPager.getCurrentItem()));
+//
+//        //保存ボタン後の処理
+//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //保存ボタン後の処理
+        ViewPager vpPager = (ViewPager) findViewById(R.id.pager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
     }
 
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return FirstFragment.newInstance(0, "Page # 1");
+                case 1: // Fragment # 0 - This will show FirstFragment different title
+                    return FirstFragment.newInstance(1, "Page # 2");
+                case 2: // Fragment # 1 - This will show SecondFragment
+                    return FirstFragment.newInstance(2, "Page # 3");
+//                    return null;
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
+        }
+
+    }
 
 }
