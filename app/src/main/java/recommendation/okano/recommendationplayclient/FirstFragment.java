5f82d133ec4c1536fe1f48c904f0b5e733bcd5ee
ttt242242 <okano565656@gmail.com>
2016-02-29 15:36:55 +0900

    とりあえず、DBに保存するコードを書いてみた。まだテスト前

--- app/src/main/java/recommendation/okano/recommendationplayclient/FirstFragment.java
+++ app/src/main/java/recommendation/okano/recommendationplayclient/FirstFragment.java
@@ -1,11 +1,16 @@
 package recommendation.okano.recommendationplayclient;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by okano on 16/02/23.
@@ -14,6 +19,8 @@ public class FirstFragment extends Fragment {
     // Store instance variables
     private String title;
     private int page;
+    private MyDBHelper myDBHelper ;
+    private SQLiteDatabase db;
 
     // newInstance constructor for creating fragment with arguments
     public static FirstFragment newInstance(int page, String title) {
@@ -29,8 +36,10 @@ public class FirstFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        page = getArguments().getInt("someInt", 0);
-        title = getArguments().getString("someTitle");
+        //page = getArguments().getInt("someInt", 0);
+        //title = getArguments().getString("someTitle");
+        myDBHelper = (MyDBHelper) getArguments().getSerializable("dbHelper");
+        db = myDBHelper.getWritableDatabase();
     }
 
     // Inflate the view for the fragment based on layout XML
@@ -39,7 +48,21 @@ public class FirstFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment0, container, false);
         TextView tvLabel = (TextView) view.findViewById(R.id.play_name);
-        tvLabel.setText(page + " -- " + title);
+        tvLabel.setText(" -- ");
+
+        // ボタンクリック　→　DBに保存
+        Button saveButton = (Button) view.findViewById(R.id.save_memo);
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+                values.put("date","test");
+                values.put("playName","test");
+                values.put("num",2);
+                values.put("memo","test");
+                db.insert("playDB", null, values);
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/recommendation/okano/recommendationplayclient/MainActivity.java
+++ app/src/main/java/recommendation/okano/recommendationplayclient/MainActivity.java
@@ -1,5 +1,6 @@
 package recommendation.okano.recommendationplayclient;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,39 +12,30 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    FragmentPagerAdapter adapterViewPager;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        FragmentManager manager = getSupportFragmentManager();
-//        manager.findFragmentByTag("android:switcher:");
-//        ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
-//        MyFragmentStatePagerAdapter adapter = new MyFragmentStatePagerAdapter(manager);
-//        viewPager.setAdapter(adapter);
-//        //viewPager.setCurrentItem(0);
-//        Log.e("ddd", String.valueOf(viewPager.getCurrentItem()));
-//
-//        //保存ボタン後の処理
-//    }
-
+    private FragmentPagerAdapter adapterViewPager;
+    private MyDBHelper myDBHelper ;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDBHelper = new MyDBHelper(this);
+        db = myDBHelper.getWritableDatabase();
+
         ViewPager vpPager = (ViewPager) findViewById(R.id.pager);
-        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager(),myDBHelper);
         vpPager.setAdapter(adapterViewPager);
     }
 
 
     public static class MyPagerAdapter extends FragmentPagerAdapter {
         private static int NUM_ITEMS = 3;
-
-        public MyPagerAdapter(FragmentManager fragmentManager) {
+        private MyDBHelper myDBHelper;
+        public MyPagerAdapter(FragmentManager fragmentManager,MyDBHelper myDBHelper) {
             super(fragmentManager);
+            this.myDBHelper = myDBHelper;
         }
 
         // Returns total number of pages
@@ -57,7 +49,12 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             switch (position) {
                 case 0: // Fragment # 0 - This will show FirstFragment
-                    return FirstFragment.newInstance(0, "Page # 1");
+                   Bundle bundle = new Bundle() ;
+                   bundle.putSerializable("dbHelper",myDBHelper);
+                    //Fragment memoFrag = FirstFragment.newInstance(0, "Page # 1");
+                    Fragment memoFrag = new FirstFragment();
+                    memoFrag.setArguments(bundle) ;
+                   return memoFrag;
                 case 1: // Fragment # 0 - This will show FirstFragment different title
                     return FirstFragment.newInstance(1, "Page # 2");
                 case 2: // Fragment # 1 - This will show SecondFragment
